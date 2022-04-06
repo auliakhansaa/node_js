72715b0fc753fe67ba7432b1c0d235e2e970fbee
auliakhansaaa <38196380+auliakhansaaa@users.noreply.github.com>
2022-04-06 23:43:39 +0700

    1

---
+++ index.js
@@ -0,0 +1,7 @@
+// console.log('Hi Love');
+
+const os = require("os");
+const luasSegitiga=require("./segitiga");
+
+// console.log("free memory:" , os.freemem());
+console.log(luasSegitiga(10,30)); 
---
+++ segitiga.js
@@ -0,0 +1,7 @@
+function luasSegitiga(alas,tinggi){
+return(alas*tinggi/2);
+}
+
+
+
+module.exports = luasSegitiga;
